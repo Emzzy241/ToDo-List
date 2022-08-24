5b58951d97eb14bc071860587c78086157790683
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-24 22:29:07 +0100

    the code for the mock database written

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@
 // A test for the business logic functions, the databases and constructors would be written in the README file before proceeding to the 
 //  uI logic and its functions
 
+// We need a database to store all of the information we get from the user, Let us make a mock one now
+
+function ToDoList(){
+    this.tasks = {};
+
+}
+
+// Now the database is created, we can easily add things into it by 
+
+
 // The first constructor is for hoe we will be adding in new tasks, a text would be written for it and it must be passed
 
 
