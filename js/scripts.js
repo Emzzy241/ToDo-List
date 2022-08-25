eacbefd4a48962d0942dc4872ad3f05b3244dcfa
Emzzy241 <emzzyoluwole@gmail.com>
2022-08-25 04:00:20 +0100

    done with writing the UI logic, time to fix some bugs

--- js/scripts.js
+++ js/scripts.js
@@ -72,3 +72,143 @@ let allTasks = new ToDoList();
 
 // The userInterface logic for my application
 
+// Function for editting TO-DO-List
+
+function editTask(id){
+    let theTasks = ToDoList.findTask(id);
+
+    $("input#personsName").val(theTasks.personsName);
+    $("input#taskName").val(theTasks.personsTask);
+    $("input#taskDate").val(theTasks.dateOfTask);
+    $("input#timeTaskTakes").val(theTasks.timeTaskWillTake);
+    $("input#briefDescriptionAboutTask").val(brieflyDescribeTask);
+
+    allTasks.deleteTask(id);
+    $("#all-tasks-show").hide();
+}
+
+// A function to attach event listeners to our tasks
+// so we want to attach event listeners for when we click on li it displays our tasks
+// we could do that in vanilla javaScript by using onClick but jQuery seems easy
+
+
+function attachTaskListeners(){
+    $("ul#show-tasks").on("click", "li", function(){
+        showTasks(this.id);
+    });
+
+    // function for deleting tasks( this function was still created later on, we just added event listeners to it here)
+
+    $("#deleteTaskButton").on("click", ".deleteButton", function(){
+        allTasks.deleteTask(this.id);
+        $("#all-tasks-show").hide();
+        displayTask(allTasks);
+    });
+
+    // function for editting tasks( this function was still created later on, we just added event listeners to it here)
+
+    $("#editTaskButton").on("click", ".editButton", function(){
+        editTask(this.id);
+    });
+}
+
+// A powerful function that will take in object as a parameter and 
+// bring out a and return a string of formatted li of all the contacts
+// in ToDoList... this function was written in the uI logic 
+// because we need our jQuery to select it 
+
+function displayTask(displayToDoList){
+    let myToDoList = $("ul#show-tasks");
+    let htmlForTasksInfo = "";
+    Object.keys(displayToDoList.tasks).forEach( function(key){
+        const oneTask = displayToDoList.findTask(key);
+
+        htmlForTasksInfo += "<li id=" + task.id + ">" + task.personsTask + "  task " + "</li>";
+    });
+    console.log(htmlForTasksInfo);
+    myToDoList(htmlForTasksInfo);
+}
+
+// function for showTasks()
+
+function showTasks(taskId){
+    const oneTask = ToDoList.findTask(taskId);
+
+    $("#all-tasks-show").show();
+    $(".usersName-show").html(task.personsName);
+    $(".nameOfTask-show").html(task.personsTask);
+    $(".dateOfTask-show").html(task.dateOfTask);
+    $(".timeToCompleteTask-show").html(task.timeTaskWillTake);
+    $(".describeTask-show").html(task.brieflyDescribeTask);
+
+    // buttons for edit and delete
+    let delButton = $("#deleteTaskButton");
+    delButton.empty();
+    delButton.html("<button class='deleteButton btn btn-danger' id=" + task.id + ">Delete</button>");
+
+    let edButton = $("#editTaskButton");
+    edButton.empty();
+    edButton.html("<button class='editBUtton btn btn-success' id=" + task.id + ">Edit</button>");
+}
+
+// Function for resetting fields
+
+function resetFields(){
+    // resetting the fields in the form when user clicks on submit or edit
+    $("input#personsName").val();
+    $("input#taskName").val();
+    $("input#taskDate").val();
+    $("input#timeTaskTakes").val();
+    $("input#briefDescriptionAboutTask").val();
+
+}
+
+
+// preventing default when user submits and saving our tasks in the UI logic
+
+$(document).ready( function(){
+    attachTaskListeners();
+
+    $("#tasks-form").submit( function(e){
+        e.preventDefault();
+
+        let nameOfUser = $("#personsName").val();
+        let taskOfUser = $("#taskName").val();
+        let usersTaskDate = $("#taskDate").val();
+        let timeTakesUser = $("#timeTaskTakes").val();
+        let briefDescriptionOfTask = $("#briefDescriptionAboutTask").val();
+
+        $("#personsName").val("");
+        $("#taskName").val("");
+        $("#taskDate").val("");
+        $("#timeTaskTakes").val("");
+        $("#briefDescriptionAboutTask").val("");
+
+        // making new objects to save our task details in our UI logic
+
+        let newTasks = new Task(nameOfUser, taskOfUser, usersTaskDate, timeTakesUser, briefDescriptionOfTask);
+
+
+        // calling on the ToDoList variabe instantiator
+
+        allTasks.addTask(newTasks);
+        console.log(allTasks.newTasks);
+
+        displayTask(allTasks);
+
+        $(".task").last().click( function(){
+            $("#all-tasks-show").show();
+            $(".usersName-show").text(newTasks.nameOfUser);
+            $(".nameOfTask-show").text(newTasks.taskOfUser);
+            $(".dateOfTask-show").text(newTasks.usersTaskDate);
+            $(".timeToCompleteTask-show").text(newTasks.timeTakesUser);
+            $(".describeTask-show").text(newTasks.briefDescriptionOfTask);
+
+        });
+
+        resetFields();
+        e.preventDefault();
+        console.log(newTasks);
+
+    });
+});
